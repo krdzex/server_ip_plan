e713439725d1c8c38f00341446fcef6c12ae7d1c
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 19:14:57 +0200

    Addin controller and route for update server

--- src/web/controllers/server.controller.ts
+++ src/web/controllers/server.controller.ts
@@ -1,14 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { ServerService } from "../../logic/services/server.service";
+import { ServerUpdateDto } from "../../data/dtos/servers/update-server.dto";
 
 export class ServerController {
   constructor(private readonly serverService: ServerService) {}
 
-  async getServerById(req: Request, res: Response, next: NextFunction) {
+  async getServerById(req: Request, res: Response) {
     const id = Number(req.params.id);
 
     const server = await this.serverService.getServerById(id);
 
     return res.status(200).send(server);
+  }
+
+  async updateServer(req: Request, res: Response, next: NextFunction) {
+    try {
+      const serverUpdate: ServerUpdateDto = req.body;
+      const { id } = req.params;
+      await this.serverService.updateServer(Number(id), serverUpdate);
+      res.status(200).json({ message: "Server updated successfully" });
+    } catch (err) {
+      next(err);
+    }
   }
 }
--- src/web/routes/server.routes.ts
+++ src/web/routes/server.routes.ts
@@ -4,5 +4,6 @@ import { serverController } from "../dependecies";
 const serverRouter = express.Router();
 
 serverRouter.get("/:id", serverController.getServerById.bind(serverController));
+serverRouter.put("/:id", serverController.updateServer.bind(serverController));
 
 export { serverRouter };
