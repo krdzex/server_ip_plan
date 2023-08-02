e55f446aac0efe5f59da604c476f39b10e4c22ea
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 21:58:21 +0200

    Adding method inside interface

--- src/data/interfaces/ip-plan-repository.interface.ts
+++ src/data/interfaces/ip-plan-repository.interface.ts
@@ -1,7 +1,9 @@
 import { CreateIpPlanDto } from "../dtos/ip-plans/create-ip-plan.dto";
 import { IpPlanDto } from "../dtos/ip-plans/ip-plan.dto";
+import { UpdateIpPlanDto } from "../dtos/ip-plans/update-ip-plan.dto";
 
 export interface IIpPlanRepository {
   getIpPlanById(id: number): Promise<IpPlanDto | null>;
   createIpPlan(ipPlan: CreateIpPlanDto): Promise<void>;
+  updateIpPlan(id: number, ipPlanUpdate: UpdateIpPlanDto): Promise<void>;
 }
