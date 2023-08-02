9fb227487b94f363f2a087d10a47db0c8d5c7b4d
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 20:11:45 +0200

    Add method in interface

--- src/data/interfaces/i-server-repository.ts
+++ src/data/interfaces/i-server-repository.ts
@@ -1,3 +1,4 @@
+import { CreateServerDto } from "../dtos/servers/create-server.dto";
 import { ServerWithPlanDto } from "../dtos/servers/server-with-plan.dto";
 import { ServerUpdateDto } from "../dtos/servers/update-server.dto";
 
@@ -5,4 +6,5 @@ export interface IServerRepository {
     getServerById(id: number): Promise<ServerWithPlanDto | null>;
     updateServer(id: number, serverUpdate: ServerUpdateDto): Promise<void>;
     deleteServer(id: number): Promise<void>;
+    createServer(server: CreateServerDto): Promise<void>;
 }
