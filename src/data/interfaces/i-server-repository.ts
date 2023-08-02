f021bf531969fd7053937c54a1381c238cf74ba7
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 19:11:07 +0200

    Add method in interface and call it inside service

--- src/data/interfaces/i-server-repository.ts
+++ src/data/interfaces/i-server-repository.ts
@@ -1,5 +1,7 @@
 import { ServerWithPlanDto } from "../dtos/servers/server-with-plan.dto";
+import { ServerUpdateDto } from "../dtos/servers/update-server.dto";
 
 export interface IServerRepository {
     getServerById(id: number): Promise<ServerWithPlanDto | null>;
+    updateServer(id: number, serverUpdate: ServerUpdateDto): Promise<void>;
 }
--- src/logic/services/server.service.ts
+++ src/logic/services/server.service.ts
@@ -1,5 +1,6 @@
 import { IServerRepository } from "../../data/interfaces/i-server-repository";
 import { ServerWithPlanDto } from "../../data/dtos/servers/server-with-plan.dto";
+import { ServerUpdateDto } from "../../data/dtos/servers/update-server.dto";
 
 export class ServerService {
   constructor(private readonly _serverRepo: IServerRepository) {}
@@ -12,5 +13,15 @@ export class ServerService {
     }
 
     return foundServer;
+  }
+
+  async updateServer(id: number, serverUpdate: ServerUpdateDto): Promise<void> {
+    const foundServer = await this._serverRepo.getServerById(id);
+
+    if(!foundServer){
+        throw new Error("No server with this id");
+    }
+
+    return await this._serverRepo.updateServer(id, serverUpdate);
   }
 }
