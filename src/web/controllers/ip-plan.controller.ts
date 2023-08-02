69924ffeae7b73f812ddbe261b3e124e466257a7
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 21:48:15 +0200

    Adding route and controller method for creating ip plan

--- src/web/controllers/ip-plan.controller.ts
+++ src/web/controllers/ip-plan.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { IpPlanService } from "../../logic/services/ip-plan.service";
+import { CreateIpPlanDto } from "../../data/dtos/ip-plans/create-ip-plan.dto";
 
 export class IpPlanController {
   constructor(private readonly ipPlanService: IpPlanService) {}
@@ -10,5 +11,15 @@ export class IpPlanController {
     const server = await this.ipPlanService.getIpPlanById(id);
 
     return res.status(200).send(server);
+  }
+
+  async createIpPlan(req: Request, res: Response, next: NextFunction) {
+    try {
+      const server: CreateIpPlanDto = req.body;
+      await this.ipPlanService.createIpPlan(server);
+      res.status(201).json("Ip plan created");
+    } catch (err) {
+      next(err);
+    }
   }
 }
--- src/web/routes/ip-plan.routes.ts
+++ src/web/routes/ip-plan.routes.ts
@@ -4,5 +4,6 @@ import { ipPlanController } from "../dependecies";
 const ipPlanRouter = express.Router();
 
 ipPlanRouter.get("/:id", ipPlanController.getIpPlanById.bind(ipPlanController));
+ipPlanRouter.post("/", ipPlanController.createIpPlan.bind(ipPlanController));
 
 export { ipPlanRouter };
