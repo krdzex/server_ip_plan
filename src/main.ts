f58384ad8ff8bdd059c1a51f62bbfee12e3aba2e
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 16:49:42 +0200

    adding router in out application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import bodyParser from "body-parser";
 import express from "express";
+import { serverRouter } from "./web/routes/server.routes";
 
 function boostrap() {
   const app = express();
 
   app.use(bodyParser.json());
+  app.use("/servers", serverRouter);
 
   const port = process.env.PORT || 3000
   app.listen(port, () => {
