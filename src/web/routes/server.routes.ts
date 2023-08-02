e8b98f41218bd4be28d820b8cb9e2bb3c8f92fae
Krsto Kostic <kostic.krsto@gmail.com>
2023-08-02 22:44:05 +0200

    Adding validation middleware

--- src/web/routes/server.routes.ts
+++ src/web/routes/server.routes.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { serverController } from "../dependecies";
+import { ValidateRequestMiddleware } from "../middlewares/validate-request.middleware";
+import { CreateServerDto } from "../../data/dtos/servers/create-server.dto";
 
 const serverRouter = express.Router();
 
 serverRouter.get("/:id", serverController.getServerById.bind(serverController));
 serverRouter.put("/:id", serverController.updateServer.bind(serverController));
-serverRouter.delete("/:id", serverController.deleteServer.bind(serverController));
-serverRouter.post("/", serverController.createServer.bind(serverController));
+serverRouter.delete(
+  "/:id",
+  serverController.deleteServer.bind(serverController)
+);
+serverRouter.post(
+  "/",
+  ValidateRequestMiddleware.with(CreateServerDto),
+  serverController.createServer.bind(serverController)
+);
 
 export { serverRouter };
